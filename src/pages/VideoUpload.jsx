8748a922da904ba5a3f442dbc7758d6e1a2c57b7
mukul <mukul.atreya@gmail.com>
2022-08-06 01:13:07 +0545

    video upload
    simple upload no ui

--- src/pages/VideoUpload.jsx
+++ src/pages/VideoUpload.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import { Web3Storage } from "web3.storage";
+import { API_TOKEN } from "../utils/constants";
+import ReactPlayer from "react-player";
+import { Box } from "@mui/material";
 
 const VideoUpload = () => {
+  const [video, setVideo] = useState(null);
+  const [videoid, setVideoId] = useState("");
+  const [videoname, setVideoname] = useState("");
+
+  function getFiles(event) {
+    event.preventDefault();
+    const fileInput = event.target;
+    setVideo(fileInput.files[0]);
+
+    // videoid = storeFiles(fileInput.files);
+    // videoname = fileInput.files[0].name;
+  }
+
+  function makeStorageClient() {
+    return new Web3Storage({ token: API_TOKEN });
+  }
+  async function storeFiles() {
+    try {
+      const client = makeStorageClient();
+      const cid = await client.put([video]);
+      console.log("stored files with cid:", cid);
+      return cid;
+    } catch (error) {
+      console.log("Error storing files:", error);
+    }
+  }
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div>Video Upload</div>;
+      <input type="file" name="video" onChange={getFiles}></input>
+      <button onClick={storeFiles}>Upload</button>
+      {
+        // TODO: add react player to automatically play upladede video
+      }
+      <div>Watch Video</div>
+      {
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <ReactPlayer
+            controls
+            url={`https://ipfs.io/ipfs/${videoid}/${videoname}`}
+          />
+        </Box>
+      }
     </>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,4 @@ import abi from "./palpal.json";
 
 export const contractAddress = "0xcontractAddress";
 export const contractABI = abi.abi;
+export const API_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEFlNTNkMWM5QzQxNDQ1NzgwMUI5NUEyMzQxZEQwMTAxYWZhOTI1MUMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTk3MTQyMzM0MTksIm5hbWUiOiJ0ZXN0In0.tZ1cpe4MDtdIA4CJkbX_MhoCTJDaruC4YTX5a4W9GxE"
