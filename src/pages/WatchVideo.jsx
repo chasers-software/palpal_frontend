caf2f8ae31d1f64c01a6ab99f30a37bed8abe8da
Sandeep Acharya <acharyas378@gmail.com>
2022-08-06 00:37:42 +0545

    Video player page simple design

--- src/pages/WatchVideo.jsx
+++ src/pages/WatchVideo.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import Header from "../components/Header";
 import ReactPlayer from "react-player";
-import { Box } from "@mui/material";
+import { Box, IconButton, Button, Typography } from "@mui/material";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import CommentIcon from "@mui/icons-material/Comment";
 
 const WatchVideo = () => {
   return (
     <>
       <Header />
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <ReactPlayer
-          controls
-          url="https://ipfs.io/ipfs/bafybeih5ggkymjjsxisyqpvavqzhmmvygtdtrbu5s4hmprhrnjicxo5lau/2.mp4"
-        />
+      <Box p={5} sx={{ display: "flex", flexDirection: "row" }}>
+        <Box
+          p={3}
+          sx={{
+            flex: 0.7,
+          }}
+        >
+          <ReactPlayer
+            width="100%"
+            controls
+            url="https://ipfs.io/ipfs/bafybeih5ggkymjjsxisyqpvavqzhmmvygtdtrbu5s4hmprhrnjicxo5lau/2.mp4"
+          />
+          <Box pt={2}>
+            <Typography variant="h4">Video Title</Typography>
+          </Box>
+          <Box mt={2} sx={{ display: "flex", justifyContent: "space-evenly" }}>
+            <Box>
+              <IconButton onClick={console.log("increase like")}>
+                <ThumbUpIcon />
+              </IconButton>
+              <Typography component="span">Like Count</Typography>
+            </Box>
+
+            <Button color="primary" variant="contained" size="small">
+              Tip Creator
+            </Button>
+          </Box>
+        </Box>
+        <Box p={3} ml={5} sx={{ flex: 0.3 }}>
+          <IconButton onClick={console.log("increase like")}>
+            <CommentIcon />
+          </IconButton>
+          <Typography>Comments</Typography>
+        </Box>
       </Box>
     </>
   );
