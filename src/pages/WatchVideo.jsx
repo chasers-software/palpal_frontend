d57abdc4df7cd13852ee06481e2a9f4d4fd0cca3
Sandeep Acharya <acharyas378@gmail.com>
2022-08-05 22:45:51 +0545

    Play videos directly from ipfs using react-player

--- src/pages/WatchVideo.jsx
+++ src/pages/WatchVideo.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
+import ReactPlayer from "react-player";
+import { Box } from "@mui/material";
 
 const WatchVideo = () => {
   return (
     <>
       <Header />
-      <div>Watch Video</div>;
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <ReactPlayer
+          controls
+          url="https://ipfs.io/ipfs/bafybeih5ggkymjjsxisyqpvavqzhmmvygtdtrbu5s4hmprhrnjicxo5lau/2.mp4"
+        />
+      </Box>
     </>
   );
 };
