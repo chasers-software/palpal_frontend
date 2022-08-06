b57e244a2cf12ee3d0147c3e4d5aafd8bf464f3b
mukul <mukul.atreya@gmail.com>
2022-08-06 10:38:16 +0545

    video play after uploading

--- src/pages/VideoUpload.jsx
+++ src/pages/VideoUpload.jsx
@@ -4,7 +4,13 @@ import { Web3Storage } from "web3.storage";
 import { API_TOKEN } from "../utils/constants";
 import ReactPlayer from "react-player";
 import { Box } from "@mui/material";
-
+import CircularProgress from '@mui/material/CircularProgress';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 const VideoUpload = () => {
   const [video, setVideo] = useState(null);
   const [videoid, setVideoId] = useState("");
@@ -14,6 +20,7 @@ const VideoUpload = () => {
     event.preventDefault();
     const fileInput = event.target;
     setVideo(fileInput.files[0]);
+    setVideoname(fileInput.files[0].name);
 
     // videoid = storeFiles(fileInput.files);
     // videoname = fileInput.files[0].name;
@@ -26,6 +33,7 @@ const VideoUpload = () => {
     try {
       const client = makeStorageClient();
       const cid = await client.put([video]);
+      setVideoId(cid);
       console.log("stored files with cid:", cid);
       return cid;
     } catch (error) {
@@ -34,21 +42,39 @@ const VideoUpload = () => {
   }
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div>Video Upload</div>;
+      
       <input type="file" name="video" onChange={getFiles}></input>
       <button onClick={storeFiles}>Upload</button>
       {
         // TODO: add react player to automatically play upladede video
       }
       <div>Watch Video</div>
-      {
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
+      {videoid?    <Box sx={{ display: "flex", justifyContent: "center" }}>
           <ReactPlayer
             controls
             url={`https://ipfs.io/ipfs/${videoid}/${videoname}`}
+            light='https://placehold.it/640x360.jpg'
+
           />
-        </Box>
+        </Box> : <Box sx={{ display: 'flex',justifyContent: "center" }}>
+        <ReactPlayer
+            controls
+            url={`https://ipfs.io/ipfs/bafybeig4u7vl7l26umcebftwy55qh7fi5y4w7pgysrshfdf3iqelej5bpi/Venice_5.mp4`}
+            light='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
+
+          />
+    </Box>}
+
+
+      {
+        // <Box sx={{ display: "flex", justifyContent: "center" }}>
+        //   <ReactPlayer
+        //     controls
+        //     url={`https://ipfs.io/ipfs/${videoid}/${videoname}`}
+        //   />
+        // </Box>
       }
     </>
   );
