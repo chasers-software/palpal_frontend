88ca476c4f26415e1b771c9c712cafe992d9c79e
mukul <mukul.atreya@gmail.com>
2022-08-06 12:37:37 +0545

    testing video upload to blockchain

--- src/pages/VideoUpload.jsx
+++ src/pages/VideoUpload.jsx
@@ -4,78 +4,134 @@ import { Web3Storage } from "web3.storage";
 import { API_TOKEN } from "../utils/constants";
 import ReactPlayer from "react-player";
 import { Box } from "@mui/material";
-import CircularProgress from '@mui/material/CircularProgress';
-import Paper from '@mui/material/Paper';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+
 const VideoUpload = () => {
   const [video, setVideo] = useState(null);
   const [videoid, setVideoId] = useState("");
   const [videoname, setVideoname] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [thumbnail, setThumbnail] = useState("");
+  const [thumbnailName, setThumbnailName] = useState("");
+  const [thumbnailId, setThumbnailId] = useState("");
 
   function getFiles(event) {
     event.preventDefault();
     const fileInput = event.target;
     setVideo(fileInput.files[0]);
     setVideoname(fileInput.files[0].name);
-
-    // videoid = storeFiles(fileInput.files);
-    // videoname = fileInput.files[0].name;
+  }
+  function getThumbnail(event) {
+    event.preventDefault();
+    const fileInput = event.target;
+    setThumbnailName(fileInput.files[0].name);
+    setThumbnail(fileInput.files[0]);
   }
 
   function makeStorageClient() {
     return new Web3Storage({ token: API_TOKEN });
   }
+  //upload title, thumbnail, video in ipfs
   async function storeFiles() {
     try {
       const client = makeStorageClient();
       const cid = await client.put([video]);
       setVideoId(cid);
       console.log("stored files with cid:", cid);
+      storeThumbnail();
+
+      const blockchainMetadata = {
+        _contentHash: cid + `/${videoname}`,
+        _thumbnailHash: thumbnailId + `/${thumbnailName}`,
+        _detailsHash: {
+          title: title,
+          description: description,
+          uploadDate: new Date(),
+        },
+      };
+
+      const stringifiedMetadata = JSON.stringify(blockchainMetadata);
+      console.log("Sttringified metadata:", stringifiedMetadata);
+
       return cid;
     } catch (error) {
       console.log("Error storing files:", error);
     }
   }
+
+  async function storeThumbnail() {
+    try {
+      const client = makeStorageClient();
+      const cid = await client.put([thumbnail]);
+      setThumbnailId(cid);
+      console.log("thubnail cid:", cid);
+      console.log("TITLE:", title);
+
+      // some function to store on blockchain
+
+
+      return cid;
+    } catch (error) {
+      console.log("Error storing thumbnail:", error);
+    }
+  }
+
   return (
     <>
       <Header />
-      <div>Video Upload</div>;
-      
-      <input type="file" name="video" onChange={getFiles}></input>
-      <button onClick={storeFiles}>Upload</button>
-      {
-        // TODO: add react player to automatically play upladede video
-      }
+      <div>
+        <div>Video Upload</div>;
+        <input
+          type="file"
+          accept="video/*"
+          name="video"
+          onChange={getFiles}
+        ></input>
+        Choose thumbnail
+        <input
+          type="file"
+          accept="image/*"
+          name="video"
+          onChange={getThumbnail}
+        ></input>
+        <TextField
+          label="Video Title"
+          variant="outlined"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          autoComplete="off"
+          inputProps={{ style: { fontSize: 15 } }}
+          InputLabelProps={{ style: { fontSize: 15, color: "GrayText" } }}
+        />
+        <TextField
+          label="Video Description"
+          variant="outlined"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          autoComplete="off"
+          inputProps={{ style: { fontSize: 15 } }}
+          InputLabelProps={{ style: { fontSize: 15, color: "GrayText" } }}
+        />
+        <button onClick={storeFiles}>Upload</button>
+      </div>
+
       <div>Watch Video</div>
-      {videoid?    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      {videoid && thumbnailId ? (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
           <ReactPlayer
             controls
             url={`https://ipfs.io/ipfs/${videoid}/${videoname}`}
-            light='https://placehold.it/640x360.jpg'
-
+            light={`https://ipfs.io/ipfs/${thumbnailId}/${thumbnailName}`}
           />
-        </Box> : <Box sx={{ display: 'flex',justifyContent: "center" }}>
-        <ReactPlayer
-            controls
-            url={`https://ipfs.io/ipfs/bafybeig4u7vl7l26umcebftwy55qh7fi5y4w7pgysrshfdf3iqelej5bpi/Venice_5.mp4`}
-            light='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
-
-          />
-    </Box>}
-
-
-      {
-        // <Box sx={{ display: "flex", justifyContent: "center" }}>
-        //   <ReactPlayer
-        //     controls
-        //     url={`https://ipfs.io/ipfs/${videoid}/${videoname}`}
-        //   />
-        // </Box>
-      }
+        </Box>
+      ) : (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress />
+        </Box>
+      )}
     </>
   );
 };
